5e08c09c75d3ac9a6de233d2f0bc25c56a00063d
Marco Rolappe <215132+mrolappe@users.noreply.github.com>
2024-07-28 13:29:56 +0200

    deps: Add :antlr4-intellij-adaptor

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ repositories {
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
 //    implementation(libs.exampleLibrary)
+    implementation("org.antlr:antlr4-intellij-adaptor:0.1")
 }
 
 // Set the JVM language level used to build the project.
