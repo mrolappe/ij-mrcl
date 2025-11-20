d6e4bc26c5c2b0f1222fa48dc9e63abbf4f3e7c0
Marco Rolappe <215132+mrolappe@users.noreply.github.com>
2025-11-20 19:26:18 +0100

    WIP: Completion contributor

---
+++ src/main/kotlin/de/rholambdapi/mrcl/intellij/language/MrclCompletionContributor.kt
@@ -0,0 +1,22 @@
+package de.rholambdapi.mrcl.intellij.language
+
+import com.intellij.codeInsight.completion.*
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.patterns.PlatformPatterns
+import com.intellij.util.ProcessingContext
+
+class MrclCompletionContributor : CompletionContributor() {
+    init {
+        extend(CompletionType.BASIC, PlatformPatterns.psiElement(),
+            object : CompletionProvider<CompletionParameters>() {
+                override fun addCompletions(
+                    parameters: CompletionParameters,
+                    context: ProcessingContext,
+                    result: CompletionResultSet
+                ) {
+                    result.addElement(LookupElementBuilder.create("tach"))
+                }
+
+            })
+    }
+}
